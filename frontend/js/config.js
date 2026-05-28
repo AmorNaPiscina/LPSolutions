6fb18014dc7afdbe6d870a2e9c9af849fcf7331a
AmorNaPiscina <gabrieljfavaro@gmail.com>
2026-05-28 10:04:21 -0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,7 +3,7 @@
 // ===========================================
 
 const CONFIG = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://lpsolutions-89zx.onrender.com',
   
   // Chaves do localStorage
   STORAGE_KEYS: {
